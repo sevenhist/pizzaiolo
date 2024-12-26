27346715874e6bbc7df1e22eaeeba5d40a6cd496
Rudnenko Dmytro <sevenhist@gmail.com>
2024-12-26 13:49:25 +0100

    routing system update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './scss/App.scss';
 import { Content } from 'pages/Content';
-import { HashRouter, Route, Routes, useSearchParams } from 'react-router-dom';
+import { Route, Routes, useSearchParams } from 'react-router-dom';
 import { ROUTES } from 'routes/routes';
 import { NotFoundPage } from 'pages/NotFoundPage';
 import { useEffect } from 'react';
@@ -15,14 +15,12 @@ function App() {
     setActiveMenuItem(paramValue ? MenuItems[paramValue] ? paramValue : "pizza" : "pizza");
   }, [searchParams])
   return (
-    <HashRouter>
       <div className="App">
         <Routes>
           <Route path={ROUTES.home} element={<Content />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
-    </HashRouter>
   );
 }
 
