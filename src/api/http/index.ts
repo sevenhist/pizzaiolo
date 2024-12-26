98ef7623d6b8a645c52a2b16f38a88a3a1cd2a84
Rudnenko Dmytro <sevenhist@gmail.com>
2024-12-26 14:12:16 +0100

    deleting credentials

--- src/api/http/index.ts
+++ src/api/http/index.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const API_URL = 'https://pizza-backend-fp0r.onrender.com/api'
 
 const $api = axios.create({
-    withCredentials: true,
     baseURL: API_URL
 })
 
