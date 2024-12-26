99e061f83a01bf1e1b5ff00b18b41b3f56dd5c9b
Rudnenko Dmytro <sevenhist@gmail.com>
2024-12-26 13:57:57 +0100

    public folder update and inluding crendetials

--- src/api/http/index.ts
+++ src/api/http/index.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export const API_URL = 'https://pizza-backend-fp0r.onrender.com/api'
 
 const $api = axios.create({
+    withCredentials: true,
     baseURL: API_URL
 })
 
