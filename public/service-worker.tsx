0ae29467c6926ed277b27f19ee802d21dc5ec11b
kalender-gmzhn <gamze.hanguel@ms.bulme.at>
2025-01-04 16:42:24 +0100

    service Worker integriert

---
+++ public/service-worker.tsx
@@ -0,0 +1,9 @@
+
+
+self.addEventListener('install', (event) => {
+  console.log('PWA installed.');
+});
+
+self.addEventListener('fetch', (event) => {
+  
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; 
+
+function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('./service-worker.tsx') 
+      .then((registration) => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  }
+}  
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+registerServiceWorker();
