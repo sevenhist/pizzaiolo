62e6f1c2b18cb8dda89eac8c5d9f80733ed310b3
Rudnenko Dmytro <sevenhist@gmail.com>
2025-01-04 15:47:00 +0100

    adding service-worker in public

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register(`${process.env.PUBLIC_URL}/service-worker.js`) // Правильный путь для публичной папки
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
 root.render(
   <React.StrictMode>
     <HashRouter>
@@ -15,7 +26,6 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
