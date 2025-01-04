caf3b8102ef6a9148647c55f84e7b373013930b4
Rudnenko Dmytro <sevenhist@gmail.com>
2025-01-04 15:56:28 +0100

    new updates

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,7 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register(`${process.env.PUBLIC_URL}/service-worker.js`) // Правильный путь для публичной папки
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
+
 root.render(
   <React.StrictMode>
     <HashRouter>
