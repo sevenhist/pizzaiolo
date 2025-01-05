09db2a0681d9677f167edc900ecd09ea1cc7817c
Rudnenko Dmytro <sevenhist@gmail.com>
2025-01-05 12:18:22 +0100

    routing update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; 
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
@@ -11,9 +11,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
