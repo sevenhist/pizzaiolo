2364ec1d4302df35ca1d81b55a2bf05e96a74126
yarakaiser <yara.kaiser@gmx.at>
2025-01-14 13:17:49 +0100

    added shopping basket image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { Logo } from "components/Logo"
 import s from "./Header.module.scss"
+import basket from "../../../img/basket.png"
 
 export const Header = () => {
     return (
@@ -7,6 +8,9 @@ export const Header = () => {
             <div className={s.header__content}>
                 <Logo />
                 <h2>Welcome to Pizzaiolo!</h2>
+            </div>
+            <div className={s.cart}>
+                <img src="../../../img/basket.png"></img>
             </div>
         </header>
     )
