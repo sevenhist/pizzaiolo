fa66c117e636c09d0f1fe3cefe752f4faddd39d1
yarakaiser <yara.kaiser@gmx.at>
2025-01-19 19:02:11 +0100

    fixed basket size; added empty click hander;

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,12 @@ import s from "./Header.module.scss"
 import basket from "../../img/basket.png"
 
 export const Header = () => {
+
+    const handleBasketClick = () => {
+        // Add click handling logic here
+        console.log('Basket clicked!');
+    };
+
     return (
         <header className={s.header}>
             <div className={s.header__content}>
@@ -10,7 +16,7 @@ export const Header = () => {
                 <h2>Welcome to Pizzaiolo!</h2>
             </div>
             <div className={s.header__basket}>
-                <button className={s.heder__bicon}>
+                <button className={s.header__icon} onClick={handleBasketClick}>
                     <img src={basket} alt="basket" />
                 </button>
             </div>
